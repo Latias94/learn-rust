2b3803556e87225f33b28ec8a7298acf57b78721
Latias94 <superfrankie621@gmail.com>
2022-03-05 21:40:35 +0800

    submit prost demo

---
+++ try-prost/src/lib.rs
@@ -0,0 +1,9 @@
+pub mod pb;
+
+// https://docs.rs/prost-build/latest/prost_build/
+// Include the `items` module, which is generated from items.proto.
+// OUT_DIR 用法
+// pub mod pb {
+//     include!(concat!(env!("OUT_DIR"), "/person.rs"));
+// }
+
