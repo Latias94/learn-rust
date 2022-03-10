e1ca14ad4f48a34dd10896f49d99e397f1755b8e
Latias94 <superfrankie621@gmail.com>
2022-03-10 21:16:57 +0800

    submit wgpu demo, chapter: Buffers and Indices, index buffer

--- try-wgpu/src/state.rs
+++ try-wgpu/src/state.rs
@@ -1,4 +1,4 @@
-use crate::vertex::{Vertex, VERTICES};
+use crate::vertex::{Vertex, INDICES, VERTICES};
 use wgpu::util::DeviceExt;
 use winit::event::{ElementState, KeyboardInput, VirtualKeyCode, WindowEvent};
 use winit::window::Window;
@@ -15,10 +15,15 @@ pub struct State {
     pub size: winit::dpi::PhysicalSize<u32>,
     pub clear_color: wgpu::Color,
     pub render_pipeline: wgpu::RenderPipeline,
-    pub challenge_render_pipeline: wgpu::RenderPipeline,
-    pub use_color: bool,
     pub vertex_buffer: wgpu::Buffer,
+    pub index_buffer: wgpu::Buffer,
     pub num_vertices: u32,
+    pub num_indices: u32,
+
+    pub challenge_vertex_buffer: wgpu::Buffer,
+    pub challenge_index_buffer: wgpu::Buffer,
+    pub num_challenge_indices: u32,
+    pub use_complex: bool,
 }
 
 impl State {
@@ -140,57 +145,52 @@ impl State {
             multiview: None,
         });
 
-        let shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
-            label: Some("Challenge Shader"),
-            source: wgpu::ShaderSource::Wgsl(include_str!("challenge.wgsl").into()),
-        });
-
-        let challenge_render_pipeline =
-            device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-                label: Some("Render Pipeline"),
-                layout: Some(&render_pipeline_layout),
-                vertex: wgpu::VertexState {
-                    module: &shader,
-                    entry_point: "vs_main",
-                    buffers: &[Vertex::desc()],
-                },
-                fragment: Some(wgpu::FragmentState {
-                    module: &shader,
-                    entry_point: "fs_main",
-                    targets: &[wgpu::ColorTargetState {
-                        format: config.format,
-                        blend: Some(wgpu::BlendState::REPLACE),
-                        write_mask: wgpu::ColorWrites::ALL,
-                    }],
-                }),
-                primitive: wgpu::PrimitiveState {
-                    topology: wgpu::PrimitiveTopology::TriangleList,
-                    strip_index_format: None,
-                    front_face: wgpu::FrontFace::Ccw,
-                    cull_mode: Some(wgpu::Face::Back),
-                    // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
-                    polygon_mode: wgpu::PolygonMode::Fill,
-                    ..Default::default()
-                },
-                depth_stencil: None,
-                multisample: wgpu::MultisampleState {
-                    count: 1,
-                    mask: !0,
-                    alpha_to_coverage_enabled: false,
-                },
-                // If the pipeline will be used with a multiview render pass, this
-                // indicates how many array layers the attachments will have.
-                multiview: None,
-            });
-
-        let use_color = true;
-
         let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Vertex Buffer"),
             contents: bytemuck::cast_slice(VERTICES), // using bytemuck to cast our VERTICES as a &[u8]
             usage: wgpu::BufferUsages::VERTEX,
         });
+
+        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(INDICES),
+            usage: wgpu::BufferUsages::INDEX,
+        });
+
         let num_vertices = VERTICES.len() as u32;
+        let num_indices = INDICES.len() as u32;
+
+        let angle = std::f32::consts::PI * 2.0 / num_vertices as f32;
+        let challenge_verts = (0..num_vertices)
+            .map(|i| {
+                let theta = angle * i as f32;
+                Vertex {
+                    position: [0.5 * theta.cos(), -0.5 * theta.sin(), 0.0],
+                    color: [(1.0 + theta.cos()) / 2.0, (1.0 + theta.sin()) / 2.0, 1.0],
+                }
+            })
+            .collect::<Vec<_>>();
+
+        let num_triangles = num_vertices as u16 - 2;
+        let challenge_indices = (1u16..num_triangles + 1)
+            .into_iter()
+            .flat_map(|i| vec![i + 1, i, 0])
+            .collect::<Vec<_>>();
+        let num_challenge_indices = challenge_indices.len() as u32;
+
+        let challenge_vertex_buffer =
+            device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                label: Some("Challenge Vertex Buffer"),
+                contents: bytemuck::cast_slice(&challenge_verts),
+                usage: wgpu::BufferUsages::VERTEX,
+            });
+        let challenge_index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Challenge Index Buffer"),
+            contents: bytemuck::cast_slice(&challenge_indices),
+            usage: wgpu::BufferUsages::INDEX,
+        });
+        let use_complex = false;
+
         Self {
             instance,
             adapter,
@@ -201,10 +201,14 @@ impl State {
             size,
             clear_color,
             render_pipeline,
-            challenge_render_pipeline,
-            use_color,
             vertex_buffer,
-            num_vertices
+            index_buffer,
+            num_vertices,
+            num_indices,
+            challenge_vertex_buffer,
+            challenge_index_buffer,
+            num_challenge_indices,
+            use_complex,
         }
     }
 
@@ -243,7 +247,7 @@ impl State {
                     },
                 ..
             } => {
-                self.use_color = *state == ElementState::Released;
+                self.use_complex = *state == ElementState::Pressed;
                 true
             }
             _ => false,
@@ -299,21 +303,34 @@ impl State {
                 // We'll use depth_stencil_attachment later, but we'll set it to None for now.
                 depth_stencil_attachment: None,
             });
-            // 前面创建了 render pipeline，这里要给 pass 设置上
-            render_pass.set_pipeline(if self.use_color {
-                &self.render_pipeline
+            let data = if self.use_complex {
+                (
+                    &self.challenge_vertex_buffer,
+                    &self.challenge_index_buffer,
+                    self.num_challenge_indices,
+                )
             } else {
-                &self.challenge_render_pipeline
-            });
+                (&self.vertex_buffer, &self.index_buffer, self.num_indices)
+            };
+
+            // 前面创建了 render pipeline，这里要给 pass 设置上
+            render_pass.set_pipeline(&self.render_pipeline);
             // One more thing: we need to actually set the vertex buffer in the render method otherwise our program will crash.
             // set_vertex_buffer takes two parameters. The first is what buffer slot to use for this vertex buffer.
             // You can have multiple vertex buffers set at a time.
             // The second parameter is the slice of the buffer to use. You can store as many objects in a buffer as your hardware allows,
             // so slice allows us to specify which portion of the buffer to use. We use .. to specify the entire buffer.
-            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            render_pass.set_vertex_buffer(0, data.0.slice(..));
+
+            render_pass.set_index_buffer(data.1.slice(..), wgpu::IndexFormat::Uint16);
+            // When using an index buffer, you need to use draw_indexed. The draw method ignores the index buffer.
+            // Also make sure you use the number of indices (num_indices), not vertices as your model will either draw wrong,
+            // or the method will panic because there are not enough indices.
+            render_pass.draw_indexed(0..data.2, 0, 0..1);
+
             // We tell wgpu to draw something with 3 vertices, and 1 instance.
             // This is where [[builtin(vertex_index)]] comes from. 我们手动传了顶点进去
-            render_pass.draw(0..self.num_vertices, 0..1);
+            // render_pass.draw(0..self.num_vertices, 0..1);
         }
 
         // submit will accept anything that implements IntoIter
